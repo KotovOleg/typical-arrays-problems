107443b0380a8b47fb65635985bd15fbc864ea9a
OlegKotov <olegkotov003@gmail.com>
2020-10-03 23:24:07 +0300

    feat:task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) { 
+    if(array === undefined || array.length === 0) {
+        return 0
+    }
+    let minArray = array[0];
+    for(let i = 0; i < array.length; i++) {
+      if (minArray > array[i]) {
+        minArray = array[i]
+      }
+    }
+  return minArray;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0) {
+        return 0
+    }
+    let maxArray = array[0];
+    for(let i = 0; i < array.length; i++) {
+        if (maxArray < array[i]) {
+            maxArray = array[i]
+        }
+    }
+    return maxArray;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum = 0;
+    if(array === undefined || !array.length) {
+        return sum
+    }
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+  return sum / array.length;
 }
+
